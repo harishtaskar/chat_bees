0e4a3e5d170c2f0a09cf0a8e5cbe4964bb54c249
Keshav3097 <harishtaskar001@gmail.com>
2024-07-06 19:17:56 +0530

    resolved build error-3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.fallback = {
+        "*": false,
+      };
+    }
+    return config;
+  },
   async redirects() {
     return [
       {
